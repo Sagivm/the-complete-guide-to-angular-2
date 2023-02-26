3fb419dd8e6977b79245788596e413c333e416e2
Sagivm <Sgmelamed@gmail.com>
2023-02-26 20:58:43 +0200

    recipe-temp

--- src/course-project/recipe/src/app/app.component.ts
+++ src/course-project/recipe/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css','../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipe';
+  context: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.context = null;
+  }
+
+  onNavItemSelected(option: string){  
+    console.log(option);
+    this.context = option;
+  }
 }
